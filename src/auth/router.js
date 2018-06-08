5b223f920c4e0769d8e3660b72d78d1a306f4884
Ben Hurst <BHurst8@gmail.com>
2018-06-08 16:55:59 -0700

    bugfix

--- src/auth/router.js
+++ src/auth/router.js
@@ -87,7 +87,7 @@ authRouter.get('/oauth/fb/code', (req, res, next) => {
 });
 
 authRouter.get('/showMeTheMoney', auth, (req, res, next) => {
-  res.redirect('https://ben-mikey-lab-web.herokuapp.com/public/main.html');
+  res.redirect('http://127.0.0.1:8080/main.html');
   // window.location.href = '../../../web-server/public/main.html';
 });
 
