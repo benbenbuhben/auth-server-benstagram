54952037bc7467339bd724b520898d90e4d846e2
Ben Hurst <BHurst8@gmail.com>
2018-06-08 19:26:13 -0700

    removed auth to test

--- src/auth/model.js
+++ src/auth/model.js
@@ -86,9 +86,9 @@ userSchema.statics.authorize = function(token) {
   console.log('token from statics.authorize', token);
   console.log('APP_SECRET from statics.authorize', process.env.APP_SECRET);
   let parsedToken = jwt.verify(token, process.env.APP_SECRET || 'changeit');
-  let query = {_id: {$oid: parsedToken.id}};
-  console.log('query from authorize w/id is', query);
-  return this.findOne(query)
+  //let query = {_id: parsedToken.id};
+  //console.log('query from authorize w/id is', query);
+  return this.findById(parsedToken.id)
     .then(user => {
       // looked up their role and then all capabilities
       console.log('user from authorize is', user);
